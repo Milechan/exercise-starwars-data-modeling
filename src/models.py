7c870235a5887ecb1ea9f07ac6f4682ea2e9b9cd
Milena Concha <milena.concha.m@gmail.com>
2025-03-07 21:02:39 +0000

    modelo listo

--- src/models.py
+++ src/models.py
@@ -6,28 +6,65 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
-    address: Mapped["Address"] = relationship(back_populates="person")
+    email: Mapped[str] = mapped_column(nullable=False)
+    password:Mapped[str]=mapped_column(nullable=False)
+
+    favorites = relationship("Favoritos", back_populates="user")
 
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Character(Base):
+    __tablename__ = 'character'
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    status: Mapped[str] = mapped_column(String, nullable=False)
+    species: Mapped[str] = mapped_column(String, nullable=False)
+    type: Mapped[str] = mapped_column(String, nullable=True)
+    gender: Mapped[str] = mapped_column(String, nullable=False)
+    origin: Mapped[str] = mapped_column(String, nullable=True)
+    location: Mapped[str] = mapped_column(String, nullable=True)
+    image: Mapped[str] = mapped_column(String, nullable=False)
+    url: Mapped[str] = mapped_column(String, nullable=False)
+    created: Mapped[str] = mapped_column(String, nullable=False)
+
+class Episode(Base):
+    __tablename__ = "episode"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    air_date: Mapped[str] = mapped_column(String, nullable=False)
+    episode: Mapped[str] = mapped_column(String, nullable=False)
+    url: Mapped[str] = mapped_column(String, nullable=False)
+    created: Mapped[str] = mapped_column(String, nullable=False)
+
+    
+
+
+class Location(Base):
+    __tablename__ ="location"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    type: Mapped[str] = mapped_column(String, nullable=False)
+    dimension: Mapped[str] = mapped_column(String, nullable=False)
+    url: Mapped[str] = mapped_column(String, nullable=False)
+    created: Mapped[str] = mapped_column(String, nullable=False)
+
+
+class Favoritos(Base):
+    __tablename__ ="favoritos"
     id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
-    person_id: Mapped[int] = mapped_column(ForeignKey("person.id"))
-    person: Mapped["Person"] = relationship(back_populates="address")
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    character_id: Mapped[int] = mapped_column(ForeignKey("character.id"), nullable=True)
+    location_id: Mapped[int] = mapped_column(ForeignKey("location.id"), nullable=True)
+    episode_id: Mapped[int] = mapped_column(ForeignKey("episode.id"), nullable=True)
 
-    def to_dict(self):
-        return {}
+    user = relationship("User",back_populates="favorites")
+    character = relationship("Character")
+    location = relationship("Location")
+    episode = relationship("Episode")
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
